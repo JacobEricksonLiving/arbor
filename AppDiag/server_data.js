e272947c2bf7d47166a20c647d657ab5fc27c23a
JacobEricksonLiving <jdwyer2@corp.ericksonretirement.com>
2016-06-17 14:51:39 -0400

    Add comments to server_data.js

--- AppDiag/server_data.js
+++ AppDiag/server_data.js
@@ -1,8 +1,16 @@
+/*
+server_data.js
+
+Holds the information on each serverNode in the system as well as their connections
+Also contains method used to expand specific serverNodes
+*/
+
+//List of all the serverNodes in the system
 var serverNodes ={
 	AX_a :{'color':'green', 'label':'a'}
 }
 
-
+//List of the connections for each serverNode in the system.
 var serverConnections = {
 	AXServers : {
 		nodes:{
