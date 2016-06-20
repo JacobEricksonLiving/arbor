d41f56c936a42060d9d84debe4e077d826d1c3ac
JacobEricksonLiving <jdwyer2@corp.ericksonretirement.com>
2016-06-20 08:57:57 -0400

    Moved into js folder

---
+++ AppDiag/js/utility_methods.js
@@ -0,0 +1,54 @@
+function clip(nName1){//removes edges of a node but not the node
+	sys.pruneNode(nName1);
+    var temp = sys.addNode(nName1.name, {'color':nName1.data.color, 'shape':nName1.data.shape, 
+    	'label':nName1.data.label, 'expanded':nName1.data.expanded,
+        'parent':nName1.data.parent, 'base':nName1.data.base, 'description':nName1.data.description
+    });
+} 
+
+function displayNodes(){
+	var inputText = document.getElementById("input");
+	var nName = inputText.value;
+	switch(nName){
+		case "AX":
+			sys.graft(serverConnections.AXServers);
+			break;
+		case "expand all applications":
+			expandAllApplications();
+			break;
+		default:
+			alert("Please enter correct command");
+	}
+}  
+
+function clearNodes(){
+	var inputText = document.getElementById("input");
+	var nName = inputText.value;
+	switch(nName){
+		case "AX":
+			for(node in serverConnections.AXServers.nodes){
+				console.log(node);
+				sys.pruneNode(node);
+			}
+			break;
+		case "remove all applications":
+			removeAllApplications();
+			break;
+		default:
+			alert("Please enter correct command");
+	}
+}
+
+function expandAllApplications(){
+	for(node in applicationNodes){
+		expandApplicationNode(node);
+	}
+}
+function removeAllApplications(){
+	expandAllApplications();//fixes unexpected behavior where system is not completely expanded yet
+	for(node in applicationNodes){
+		sys.pruneNode(node);		
+	}
+	sys.addNode('AX', applicationNodes.AX);//since this are removed with the for loop add back it
+	sys.addNode('Vision', applicationNodes.Vision);
+}
