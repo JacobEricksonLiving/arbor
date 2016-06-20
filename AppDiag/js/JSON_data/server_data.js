442f308bc0a829e1fe24315a190e2d73fd5d563a
JacobEricksonLiving <jdwyer2@corp.ericksonretirement.com>
2016-06-20 12:17:34 -0400

    Add shells for each app under pp under serverNodes JSON

--- AppDiag/js/JSON_data/server_data.js
+++ AppDiag/js/JSON_data/server_data.js
@@ -7,14 +7,68 @@ Also contains method used to expand specific serverNodes
 
 //List of all the serverNodes in the system
 var serverNodes ={
-	AX_a :{'color':'green', 'label':'a'}
-}
+	AXServers:{
+		AX_a :{'color':'green', 'label':'a'}
+	},
+
+	BizTalkServers:{
+
+	},
+
+	BizTalk360Servers:{
+
+	},
+
+	CentricityServers:{
+
+	},
+
+	CofaxServers:{
+
+	},
+
+	CorepointServers:{
+
+	},
+
+	GSMSServers:{
+
+	},
+
+	HyperionServers:{
+
+	},
+
+	NetMenuServers:{
+
+	},
+
+	OdysseyServers:{
+
+	},
+
+	PeopleSoftServers:{
+
+	},
+
+	SalesForceServers:{
+
+	},
+
+	SimphonyServers:{
+
+	},
+
+	VisionServers:{
+
+	}
+}//end serverNodes
 
 //List of the connections for each serverNode in the system.
 var serverConnections = {
 	AXServers : {
 		nodes:{
-			AX_a:serverNodes.AX_a
+			AX_a:serverNodes.AXServers.AX_a
 		},
 		edges:{
 			AX:{ AX_a:{weight:2}}
