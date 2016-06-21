7984c54ebfeabbf3f92a957eba7d142943607ee1
JacobEricksonLiving <jdwyer2@corp.ericksonretirement.com>
2016-06-21 14:02:40 -0400

    Remove node name preface from servrvers Add simphony databases

--- AppDiag/js/JSON_data/server_data.js
+++ AppDiag/js/JSON_data/server_data.js
@@ -123,13 +123,13 @@ var serverConnections = {
 	CentricityServers:{
 		database:{
 			nodes:{
-				Centricity_md1_db16:{'color':'purple', 'label':'md1-db16', 'description':'Primary database'},
-				Centricity_md1_db25:{'color':'purple', 'label':'md1-db16', 'description':'Shared DB used for Secure Messaging/Patient Portal'}				
+				md1_db16:{'color':'purple', 'label':'md1-db16', 'description':'Primary database'},
+				md1_db25:{'color':'purple', 'label':'md1-db16', 'description':'Shared DB used for Secure Messaging/Patient Portal'}				
 			},
 			edges:{
 				Centricity:{
-					Centricity_md1_db16:{weight:2},
-					Centricity_md1_db25:{weight:2}
+					md1_db16:{weight:2},
+					md1_db25:{weight:2}
 				}
 			}
 		},
@@ -154,35 +154,35 @@ var serverConnections = {
 
 		production:{
 			nodes:{
-				Centricity_md1_ap10:{'color':'green', 'label':'md1-ap10', 'description':'Report Server'},
-				Centricity_md1_ap14:{'color':'green', 'label':'md1-ap14', 'description':'Utility Integration Server'},
-				Centricity_md1_ap37:{'color':'green', 'label':'md1-ap37', 'description':'ePerscription Server'},
-				Centricity_md1_ap42:{'color':'green', 'label':'md1-ap42', 'description':'Corepoint Server'},
-				Centricity_md1_ap51:{'color':'green', 'label':'md1-ap51', 'description':'Secure Messaging/Patient Portal'},
-				Centricity_md1_ap52:{'color':'green', 'label':'md1-ap52', 'description':'Houses/Configure files for DocuTrack'},
-				Centricity_md1_ap53:{'color':'green', 'label':'md1-ap53', 'description':'Data Transfer Station. Inbound'},
-				Centricity_md1_ap54:{'color':'green', 'label':'md1-ap54', 'description':'Data Transfer Station. Outbound'},
-				Centricity_md1_ap55:{'color':'green', 'label':'md1-ap55', 'description':'Data Transfer Station. Clinical'},
-				Centricity_md1_ap73:{'color':'green', 'label':'md1-ap73', 'description':'Java Boss Server. Runs Reports. Secure Messaging/Patient Portal.'},
-				Centricity_md1_ap74:{'color':'green', 'label':'md1-ap74', 'description':'java Boss Server. Corepoint Server used to export changes.'},
-				Centricity_Minnow_01:{'color':'green', 'label':'Minnow-01', 'description':'DMZ Web Server for Secure Messaging/Patient Portal'},
-				Centricity_xa102__xa117:{'color':'green', 'label':'xa102--xa117', 'description':'Some are in reserver'}
+				md1_ap10:{'color':'green', 'label':'md1-ap10', 'description':'Report Server'},
+				md1_ap14:{'color':'green', 'label':'md1-ap14', 'description':'Utility Integration Server'},
+				md1_ap37:{'color':'green', 'label':'md1-ap37', 'description':'ePerscription Server'},
+				md1_ap42:{'color':'green', 'label':'md1-ap42', 'description':'Corepoint Server'},
+				md1_ap51:{'color':'green', 'label':'md1-ap51', 'description':'Secure Messaging/Patient Portal'},
+				md1_ap52:{'color':'green', 'label':'md1-ap52', 'description':'Houses/Configure files for DocuTrack'},
+				md1_ap53:{'color':'green', 'label':'md1-ap53', 'description':'Data Transfer Station. Inbound'},
+				md1_ap54:{'color':'green', 'label':'md1-ap54', 'description':'Data Transfer Station. Outbound'},
+				md1_ap55:{'color':'green', 'label':'md1-ap55', 'description':'Data Transfer Station. Clinical'},
+				md1_ap73:{'color':'green', 'label':'md1-ap73', 'description':'Java Boss Server. Runs Reports. Secure Messaging/Patient Portal.'},
+				md1_ap74:{'color':'green', 'label':'md1-ap74', 'description':'java Boss Server. Corepoint Server used to export changes.'},
+				Minnow_01:{'color':'green', 'label':'Minnow-01', 'description':'DMZ Web Server for Secure Messaging/Patient Portal'},
+				xa102__xa117:{'color':'green', 'label':'xa102--xa117', 'description':'Some are in reserver'}
 			},
 			edges:{
 				Centricity:{
-					Centricity_md1_ap10:{weight:2},
-					Centricity_md1_ap14:{weight:2},
-					Centricity_md1_ap37:{weight:2},
-					Centricity_md1_ap42:{weight:2},
-					Centricity_md1_ap51:{weight:2},
-					Centricity_md1_ap52:{weight:2},
-					Centricity_md1_ap53:{weight:2},
-					Centricity_md1_ap54:{weight:2},
-					Centricity_md1_ap55:{weight:2},
-					Centricity_md1_ap73:{weight:2},
-					Centricity_md1_ap74:{weight:2},
-					Centricity_Minnow_01:{weight:2},
-					Centricity_xa102__xa117:{weight:2}
+					md1_ap10:{weight:2},
+					md1_ap14:{weight:2},
+					md1_ap37:{weight:2},
+					md1_ap42:{weight:2},
+					md1_ap51:{weight:2},
+					md1_ap52:{weight:2},
+					md1_ap53:{weight:2},
+					md1_ap54:{weight:2},
+					md1_ap55:{weight:2},
+					md1_ap73:{weight:2},
+					md1_ap74:{weight:2},
+					Minnow_01:{weight:2},
+					xa102__xa117:{weight:2}
 				}
 			}
 		}
@@ -267,10 +267,10 @@ var serverConnections = {
 	DocuTrackServers:{
 		database:{
 			nodes:{
-				DocuTrack_md1_db04:{'color':'purple', 'label':'md1-db04', 'description':'Shared DB'}
+				md1_db04:{'color':'purple', 'label':'md1-db04', 'description':'Shared DB'}
 			},
 			edges:{
-				DocuTrack:{DocuTrack_md1_db04:{weight:2}}
+				DocuTrack:{md1_db04:{weight:2}}
 			}
 		},
 
@@ -294,13 +294,13 @@ var serverConnections = {
 
 		production:{
 			nodes:{
-				DocuTrack_md1_xa100:{'color':'green', 'label':'md1-xa100', 'description':''},
-				DocuTrack_md1_xa101:{'color':'green', 'label':'md1-xa101', 'description':''}
+				md1_xa100:{'color':'green', 'label':'md1-xa100', 'description':''},
+				md1_xa101:{'color':'green', 'label':'md1-xa101', 'description':''}
 			},
 			edges:{
 				DocuTrack:{
-					DocuTrack_md1_xa100:{weight:2},
-					DocuTrack_md1_xa101:{weight:2}
+					md1_xa100:{weight:2},
+					md1_xa101:{weight:2}
 				}
 			}
 		}
@@ -461,10 +461,10 @@ var serverConnections = {
 	OdysseyServers:{
 		database:{
 			nodes:{
-
+				md1_db03:{'color':'purple', 'label':'md1_db03', 'description':''}
 			},
 			edges:{
-
+				Odyssey:{md1_db03:{weight:2}}
 			}
 		},
 
@@ -488,10 +488,10 @@ var serverConnections = {
 
 		production:{
 			nodes:{
-				Odyssey_md1_ap68:{'color':'green', 'label':'md1-ap68'}
+				md1_ap68:{'color':'green', 'label':'md1-ap68', 'description':''}
 			},
 			edges:{
-				Odyssey:{Odyssey_md1_ap68:{weight:2}}
+				Odyssey:{md1_ap68:{weight:2}}
 			}
 		}
 	},//end OdysseyServers
@@ -613,10 +613,10 @@ var serverConnections = {
 	SimphonyServers:{
 		database:{
 			nodes:{
-
+				md1_db25:{'color':'purple', 'label':'md1-db25', 'description':''}
 			},
 			edges:{
-
+				Simphony:{md1_db25:{weight:2}}
 			}
 		},
 
@@ -640,17 +640,17 @@ var serverConnections = {
 
 		production:{
 			nodes:{
-				Simphony_md1_ap64:{'color':'green', 'label':'md1-ap64'},
-				Simphony_md1_ap65:{'color':'green', 'label':'md1-ap65'},
-				Simphony_md1_ap66:{'color':'green', 'label':'md1-ap66'},
-				Simphony_md1_ap67:{'color':'green', 'label':'md1-ap67'},
+				md1_ap64:{'color':'green', 'label':'md1-ap64'},
+				md1_ap65:{'color':'green', 'label':'md1-ap65'},
+				md1_ap66:{'color':'green', 'label':'md1-ap66'},
+				md1_ap67:{'color':'green', 'label':'md1-ap67'},
 			},
 			edges:{
 				Simphony:{
-					Simphony_md1_ap64:{weight:2},
-					Simphony_md1_ap65:{weight:2},
-					Simphony_md1_ap66:{weight:2},
-					Simphony_md1_ap67:{weight:2}
+					md1_ap64:{weight:2},
+					md1_ap65:{weight:2},
+					md1_ap66:{weight:2},
+					md1_ap67:{weight:2}
 				}
 			}
 		}
