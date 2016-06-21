e9b461295cad9bbc6f57c4a55ee220d38850e1d9
JacobEricksonLiving <jdwyer2@corp.ericksonretirement.com>
2016-06-21 09:18:17 -0400

    Changed comments

--- AppDiag/js/JSON_data/server_data.js
+++ AppDiag/js/JSON_data/server_data.js
@@ -2,13 +2,7 @@
 server_data.js
 
 Holds the information on each serverNode in the system as well as their connections
-Also contains method used to expand specific serverNodes
 */
-
-//List of all the serverNodes in the system
-var serverNodes ={
-
-}
 
 //List of the connections for each serverNode in the system.
 var serverConnections = {
